c948b8040b40971c2ef60d77636c992ea8c067b2
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2023-12-12 00:35:14 +0100

    Added dynamic page component, componets is now created with data fron the data.json file

--- app/ui/projects/projects.jsx
+++ app/ui/projects/projects.jsx
@@ -1,46 +1,30 @@
-import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { promises as fs } from 'fs';
 
-export default function projects() {
+export default async function projects() {
+    const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
+    const data = JSON.parse(file);
+
     return (
-<div class="min-h-screen py-12 flex items-center justify-center md:flex-row flex-col">
-      
-        {/* box 1 */}
-      <div className="py-6 px-4 bg-[#545d68] text-[#ADBAC7] m-5"> 
-        {/* Header text */}
-        <div className="left-0 top-0 text-lg"> 
-        arduino_I2C_LCD
-        </div>
-        {/* About text */}
-        <div className="pt-5 max-w-[325px] text-sm">
-          This is a Arduino project. The project was about learning how the arduino I2C LCD display works. We also conected a Ultrasonic distance sensor and displayed its output on the lcd screen 
-        </div>
-        {/* Buttons */}
-        <div className="pt-4">
-        <Link target="_blank" href="https://github.com/Jepbe/HTX-repo/blob/main/robottek/arduino_I2C_LCD/HelloWorld/HelloWorld.ino">
-          <button className="border rounded border-[#ADBAC7] hover:bg-[#666f7a] bg- text-sm p-1 font-semibold inline-flex items-center"><FaGithub className="text-sm mr-[0.1rem]"/>Github</button>
-        </Link>
-        </div>
-      </div>
-
-      {/* box 2 */}
-      <div className="py-6 px-4 bg-[#545d68] text-[#ADBAC7] m-5"> 
-      {/* Header text */}
-      <div className="left-0 top-0 text-lg"> 
-      arduino_I2C_LCD
-      </div>
-      {/* About text */}
-      <div className="pt-5 max-w-[325px] text-sm">
-        This is a Arduino project. The project was about learning how the arduino I2C LCD display works. We also conected a Ultrasonic distance sensor and displayed its output on the lcd screen 
-      </div>
-      {/* Buttons */}
-      <div className="pt-4">
-      <Link target="_blank" href="https://github.com/Jepbe/HTX-repo/blob/main/robottek/arduino_I2C_LCD/HelloWorld/HelloWorld.ino">
-        <button className="border rounded border-[#ADBAC7] hover:bg-[#666f7a] bg- text-sm p-1 font-semibold inline-flex items-center"><FaGithub className="text-sm mr-[0.1rem]"/>Github</button>
-      </Link>
-      </div>
-    </div>
-
+<div className="min-h-screen py-12 flex items-center justify-center md:flex-row flex-col">
+      {data.map((data) => {
+        return (
+          <div key={data.id} className="py-6 px-4 bg-[#545d68] text-[#ADBAC7] m-5"> 
+            <div className="left-0 top-0 text-lg"> 
+              {data.title}
+            </div>
+            <div className="pt-5 max-w-[325px] text-sm">
+              {data.content}
+            </div>
+            <div className="pt-4">
+              <Link target="_blank" href={data.GitHub}>
+                <button className="border rounded border-[#ADBAC7] hover:bg-[#666f7a] bg- text-sm p-1 font-semibold inline-flex items-center"><FaGithub className="text-sm mr-[0.1rem]"/>Github</button>
+              </Link>
+            </div>
+          </div>
+        );
+      })}
 </div>
     );
 }
